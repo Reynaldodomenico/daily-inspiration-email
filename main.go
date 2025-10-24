137a8a54dccfc8a19fdc6b7c543492482ed99d22
Reynaldodomenico <Reynaldodomenico@yahoo.com>
2025-10-24 02:40:02 +0200

    add godotenv usage

--- main.go
+++ main.go
@@ -134,6 +134,10 @@ func sendDailyEmail() {
 }
 
 func main() {
+    if err := godotenv.Load(); err != nil {
+        log.Println("No .env file found, using system environment variables")
+    }
+
     sendDailyEmail()
 }
 
